ac9e59631ccba1665c97c8aa46ff16d8a58bb15d
JohnIhab <elmalomawy@gmail.com>
2024-09-30 11:03:23 +0300

    asfd

--- dist/index.js
+++ dist/index.js
@@ -5,15 +5,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = require("dotenv");
+const helmet_1 = __importDefault(require("helmet"));
+const connect_timeout_1 = __importDefault(require("connect-timeout"));
 const cors_1 = __importDefault(require("cors"));
 (0, dotenv_1.config)();
 const routers_1 = __importDefault(require("./src/routers"));
 const errorMiddleware_1 = __importDefault(require("./src/middlewares/errorMiddleware"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: true }));
-console.log(process.env.CLOUDINARY_CLOUD_NAME);
+app.use((0, helmet_1.default)());
+const corsOptions = {
+    origin: "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+    optionsSuccessStatus: 204
+};
+app.use((0, cors_1.default)(corsOptions));
+app.use((0, connect_timeout_1.default)('10m'));
+app.use(express_1.default.json({ limit: '2gb' })); // أو الحد المطلوب
+app.use(express_1.default.urlencoded({ limit: '2gb', extended: true }));
 app.use("/uploads", express_1.default.static("uploads"));
 app.use("/assets", express_1.default.static("assets"));
 app.use("/api", routers_1.default);
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import {config} from "dotenv"
+import helmet from 'helmet';
+import timeout from 'connect-timeout';
 import cors from "cors"
 config();
 import router from "./src/routers"
@@ -8,9 +10,20 @@ import globalError from "./src/middlewares/errorMiddleware"
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-console.log( process.env.CLOUDINARY_CLOUD_NAME);
+
+app.use(helmet());
+const corsOptions = {
+	origin: "*", // Replace with your frontend URL
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	allowedHeaders: 'Content-Type,Authorization',
+	credentials: true,
+	optionsSuccessStatus: 204
+}; 
+app.use(cors(corsOptions));
+app.use(timeout('10m'));
+app.use(express.json({ limit: '2gb' })); // أو الحد المطلوب
+app.use(express.urlencoded({ limit: '2gb', extended: true }));
+
 app.use("/uploads", express.static("uploads"));
 app.use("/assets", express.static("assets"));
 
